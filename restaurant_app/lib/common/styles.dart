fea7c271bf28517fda4e1fa06ef017bd5b8299b6
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2023-11-13 19:39:57 +0700

    Create styles dart page

---
+++ restaurant_app/lib/common/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color primaryColor = Color(0xffffffff);
+const Color secondaryColor = Colors.orange;
+const Color tertiaryColor = Color(0xff050224);
+
+// final TextTheme myTextTheme = TextTheme(
+//   pass;
+// )
